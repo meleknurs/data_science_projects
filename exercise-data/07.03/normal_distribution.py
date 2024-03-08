e43b64db64dd48fe7128692afb68bcf540a7aee7
Meleknur <melek.soylu@gmail.com>
2024-03-08 10:27:36 +0100

    finished normal distribution exercises

---
+++ exercise-data/07.03/normal_distribution.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+amir_deals = pd.read_csv("amir_deals.csv")
+
+
+# Histogram of amount with 10 bins and show plot
+amir_deals['amount'].hist(bins=10)
+plt.show()
+
+from scipy.stats import norm
+
+# Probability of deal < 7500
+prob_less_7500 = norm.cdf(7500, 5000, 2000)
+
+print(prob_less_7500)
+
+# Probability of deal > 1000
+prob_over_1000 = 1- norm.cdf(1000, 5000, 2000)
+
+print(prob_over_1000)
+
+# Probability of deal between 3000 and 7000
+prob_3000_to_7000 = norm.cdf(7000, 5000, 2000) - norm.cdf(3000, 5000, 2000)
+print(prob_3000_to_7000)
+
+# Calculate amount that 25% of deals will be less than
+pct_25 = norm.ppf(0.25, 5000, 2000)
+
+print(pct_25)
