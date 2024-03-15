1e2cd25d25e13be91b7e7a7ebd76083c04715095
Meleknur <melek.soylu@gmail.com>
2024-03-15 10:02:09 +0100

    finish exercises of style, hue, markers, dashes, and ci settings

--- exercise-data/15.03/customizing_scatter_plot.py
+++ exercise-data/15.03/customizing_scatter_plot.py
@@ -25,4 +25,36 @@ sns.relplot(x= "acceleration",
 
 
 # Show plot
+#plt.show()
+
+
+# Create line plot
+sns.relplot(x="model_year",
+            y= "mpg",
+            data= mpg,
+            kind= "line")
+
+
+# Show plot
+plt.show()
+
+######
+# Make the shaded area show the standard deviation
+sns.relplot(x="model_year", y="mpg",
+            data=mpg, kind="line",
+            ci="sd")
+
+# Show plot
+plt.show()
+
+
+# Add markers and make each line have the same style
+sns.relplot(x="model_year", y="horsepower", 
+            data=mpg, kind="line", 
+            ci=None, style="origin", 
+            hue="origin",
+            markers= True,
+            dashes=False)
+
+# Show plot
 plt.show()
