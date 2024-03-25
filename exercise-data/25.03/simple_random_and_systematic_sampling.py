07236f4c1e91249f1656dae6fca171a818190b2c
Meleknur <melek.soylu@gmail.com>
2024-03-25 15:28:01 +0100

    stratified and cluster sampling exercises

---
+++ exercise-data/25.03/simple_random_and_systematic_sampling.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+attrition_pop=pd.read_feather("attrition.feather")
+
+# Sample 70 rows using simple random sampling and set the seed
+attrition_samp = attrition_pop.sample(n=70, random_state=18900217)
+
+# Print the sample
+print(attrition_samp)
+
+# Set the sample size to 70
+sample_size = 70
+
+# Calculate the population size from attrition_pop
+pop_size = len(attrition_pop)
+
+# Calculate the interval
+interval = pop_size // sample_size
+
+# Systematically sample 70 rows
+
+attrition_sys_samp = attrition_pop.iloc[::interval]
+
+# Print the sample
+print(attrition_sys_samp)
+
+# Add an index column to attrition_pop
+attrition_pop_id = attrition_pop.reset_index()
+
+# Plot YearsAtCompany vs. index for attrition_pop_id
+attrition_pop_id.plot(x='index', y="YearsAtCompany", kind="scatter")
+plt.show()
+
+# Shuffle the rows of attrition_pop
+attrition_shuffled = attrition_pop.sample(frac=1)
+
+# Reset the row indexes and create an index column
+attrition_shuffled = attrition_shuffled.reset_index(drop=True).reset_index()
+
+# Plot YearsAtCompany vs. index for attrition_shuffled
+attrition_shuffled.plot(x="index", y="YearsAtCompany", kind="scatter")
+plt.show()
+
+#####
+# Proportion of employees by Education level
+education_counts_pop = attrition_pop['Education'].value_counts(normalize=True)
+
+# Print education_counts_pop
+print(education_counts_pop)
+
+# Proportional stratified sampling for 40% of each Education group
+attrition_strat = attrition_pop.groupby('Education')\
+	.sample(frac=0.4, random_state=2022)
+
+# Calculate the Education level proportions from attrition_strat
+education_counts_strat = attrition_strat['Education'].value_counts(normalize=True)
+
+# Print education_counts_strat
+print(education_counts_strat)
+
+###
+# Get 30 employees from each Education group
+attrition_eq = attrition_pop.groupby('Education')\
+	.sample(n=30, random_state=2022)      
+
+# Get the proportions from attrition_eq
+education_counts_eq = attrition_eq['Education'].value_counts(normalize=True) 
+
+# Print the results
+print(education_counts_eq)
+
+####
+
+# Plot YearsAtCompany from attrition_pop as a histogram
+attrition_pop['YearsAtCompany'].hist(bins=np.arange(0, 41, 1))
+plt.show()
+
+# Sample 400 employees weighted by YearsAtCompany
+attrition_weight = attrition_pop.sample(n=400, weights="YearsAtCompany")
+
+# Plot YearsAtCompany from attrition_weight as a histogram
+attrition_weight["YearsAtCompany"].hist(bins=np.arange(0,41,1))
+plt.show()
+
+###
+# Create a list of unique JobRole values
+job_roles_pop = list(attrition_pop['JobRole'].unique())
+
+# Randomly sample four JobRole values
+job_roles_samp = random.sample(job_roles_pop, k=4)
+
+# Print the result
+print(job_roles_samp)
+
+###
+# Create a list of unique JobRole values
+job_roles_pop = list(attrition_pop['JobRole'].unique())
+
+# Randomly sample four JobRole values
+job_roles_samp = random.sample(job_roles_pop, k=4)
+
+# Filter for rows where JobRole is in job_roles_samp
+jobrole_condition = attrition_pop['JobRole'].isin(job_roles_samp)
+attrition_filtered = attrition_pop[jobrole_condition]
+
+# Remove categories with no rows
+attrition_filtered['JobRole'] = attrition_filtered['JobRole'].cat.remove_unused_categories()
+
+# Randomly sample 10 employees from each sampled job role
+attrition_clust = attrition_filtered.groupby('JobRole').sample(n=10, random_state=2022)
+
+
+# Print the sample
+print(attrition_clust)
