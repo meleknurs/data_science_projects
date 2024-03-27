cc1a19995860e4a74ef93f74fe6b051d9bbe26e8
Meleknur <melek.soylu@gmail.com>
2024-03-27 09:53:09 +0100

    one sample and two samples proportions tests

---
+++ exercise-data/27.03/proportion_test.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from statsmodels.stats.proportion import proportions
+
+
+late_shipments=pd.read_feather("late_shipments.feather")
+
+# Hypothesize that the proportion of late shipments is 6%
+p_0 = 0.06
+
+# Calculate the sample proportion of late shipments
+p_hat = (late_shipments['late'] == "Yes").mean()
+
+# Calculate the sample size
+n = len(late_shipments)
+
+# Calculate the numerator and denominator of the test statistic
+numerator = p_hat - p_0
+denominator = np.sqrt(p_0 * (1 - p_0) / n)
+
+# Calculate the test statistic
+z_score = numerator / denominator
+
+# Calculate the p-value from the z-score
+p_value = 1- norm.cdf(z_score)
+
+# Print the p-value
+print(p_value)
+
+####
+
+# Calculate the pooled estimate of the population proportion
+p_hat = (p_hats["reasonable"] * ns["reasonable"] + p_hats["expensive"] * ns["expensive"]) / (ns["reasonable"] + ns["expensive"])
+
+# Calculate p_hat one minus p_hat
+p_hat_times_not_p_hat = p_hat * (1 - p_hat)
+
+# Divide this by each of the sample sizes and then sum
+p_hat_times_not_p_hat_over_ns = p_hat_times_not_p_hat / ns["expensive"] + p_hat_times_not_p_hat / ns["reasonable"]
+
+# Calculate the standard error
+std_error = np.sqrt(p_hat_times_not_p_hat_over_ns)
+
+# Calculate the z-score
+z_score = (p_hats["expensive"] - p_hats["reasonable"]) / std_error
+
+# Calculate the p-value from the z-score
+p_value = 1 - norm.cdf(z_score)
+
+# Print p_value
+print(p_value)
+
+###
+# Count the late column values for each freight_cost_group
+late_by_freight_cost_group = late_shipments.groupby("freight_cost_group")['late'].value_counts()
+
+# Create an array of the "Yes" counts for each freight_cost_group
+success_counts = np.array([45, 16])
+
+# Create an array of the total number of rows in each freight_cost_group
+n = np.array([45 + 500, 16 + 439])
+
+# Run a z-test on the two proportions
+stat, p_value = proportions_ztest(count=success_counts, nobs=n,
+                                  alternative="larger")
+
+# Print the results
+print(stat, p_value)
+
