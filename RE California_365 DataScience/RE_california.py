5809b84de0fde9e1277739e94f1b7ac4dde8fcdc
Meleknur <melek.soylu@gmail.com>
2024-04-09 13:57:05 +0200

    finish task 2

--- RE California_365 DataScience/RE_california.py
+++ RE California_365 DataScience/RE_california.py
@@ -4,4 +4,24 @@ import matplotlib.pyplot as plt
 
 data=pd.read_excel("re_california.xlsx", sheet_name= "365RE", skiprows=4)
 
-print(data.columns) 
+#Task 2: Create a frequency distribution graph representing the price (that is a histogram with the highest possible number of bins - 267). 
+
+price=data['Price'].to_list()
+
+#calculate bin width
+num_bins= 267
+price_range=max(price)- min(price)
+bin_width= price_range/num_bins
+
+#create bins
+bins= np.arange(min(price), max(price)+ bin_width, bin_width)
+
+#calculate frequencies
+hist, _ = np.histogram(price, bins=bins)
+
+#create an histogram
+plt.hist(price, bins=num_bins)
+plt.xlabel('Price')
+plt.ylabel('Frequency')
+plt.title('Frequency Distribution of Prices')
+plt.show()
