e5c16594780ff20d70fcffb14f22898c55a58712
Meleknur <melek.soylu@gmail.com>
2024-04-24 23:17:13 +0200

    finishexercises to categorical errors by using strip(), qcut(), cut(), mapping

---
+++ exercise-data/24.04/inconsistent_categories.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+airlines= pd.read_csv("airlines_final.csv")
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower() 
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_size'].unique())
+print(airlines['dest_region'].unique())
+
+
+###########
+# Create ranges for categories
+label_ranges = [0, 60, 180, np.inf]
+label_names = ['short', 'medium', 'long']
+
+# Create wait_type column
+airlines['wait_type'] = pd.cut(airlines['wait_min'], bins = label_ranges, 
+                                labels = label_names)
+
+# Create mappings and replace
+mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday': 'weekday', 
+            'Thursday': 'weekday', 'Friday': 'weekday', 
+            'Saturday': 'weekend', 'Sunday': 'weekend'}
+
+airlines['day_week'] = airlines['day'].replace(mappings)
