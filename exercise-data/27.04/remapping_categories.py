2f33f582ba5a67ba6119e5aac641b59bf7fae6c4
Meleknur <melek.soylu@gmail.com>
2024-04-27 16:43:01 +0200

    remapping categories with thefuzz

---
+++ exercise-data/27.04/remapping_categories.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from thefuzz import process
+
+restaurants= pd.read_csv("restaurants_L2.csv")
+
+restaurants['cuisine_type']= restaurants['type']
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian',unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit= len(unique_types)))
+
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit=len(unique_types)))
+
+# Inspect the unique values of the cuisine_type column
+print(restaurants['cuisine_type'].unique())
+
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit= len(restaurants['cuisine_type']))
+
+# Inspect the first 5 matches
+print(matches[0:5])
+
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+# Iterate through the list of matches to italian
+for match in matches:
+  # Check whether the similarity score is greater than or equal to 80
+  if match[1] >= 80:
+    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+         restaurants.loc[restaurants['cuisine_type'] == match[0], 'cuisine_type'] = 'italian'
+
+# Iterate through categories
+for cuisine in categories:  
+  # Create a list of matches, comparing cuisine with the cuisine_type column
+  matches = process.extract(cuisine, restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+  # Iterate through the list of matches
+  for match in matches:
+     # Check whether the similarity score is greater than or equal to 80
+    if match[1] >= 80:
+      # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+      restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
+      
+# Inspect the final result
+print(restaurants['cuisine_type'].unique())
+
