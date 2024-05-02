b71adad12f28a9ef66d53622132a98d1fd261568
Meleknur <melek.soylu@gmail.com>
2024-05-02 15:00:17 +0200

    group data by category in pandas

--- exercise-data/02.05/categorical_1.py
+++ exercise-data/02.05/categorical_1.py
@@ -19,3 +19,23 @@ adult2 = pd.read_csv(
   dtype= adult_dtypes
 )
 print(adult2.dtypes)
+
+
+#######
+# Group the adult dataset by "Sex" and "Above/Below 50k"
+gb = adult.groupby(by=['Sex', 'Above/Below 50k'])
+
+# Print out how many rows are in each created group
+print(gb.size())
+
+# Print out the mean of each group for all columns
+print(gb.mean())
+
+# Create a list of user-selected variables
+user_list = ["Education", "Above/Below 50k"]
+
+# Create a GroupBy object using this list
+gb = adult.groupby(by=user_list)
+
+# Find the mean for the variable "Hours/Week" for each group - Be efficient!
+print(gb["Hours/Week"].mean())
