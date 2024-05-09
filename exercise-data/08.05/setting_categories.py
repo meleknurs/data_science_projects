a10c3b46a551d33ca0db3dcfa402b9341de53366
Meleknur <melek.soylu@gmail.com>
2024-05-09 22:35:08 +0200

    excercise of removing categories by using .cat methods

--- exercise-data/08.05/setting_categories.py
+++ exercise-data/08.05/setting_categories.py
@@ -16,3 +16,20 @@ dogs["keep_in"] = dogs["keep_in"].cat.add_categories(new_categories)
 
 # Check frequency counts one more time
 print(dogs["keep_in"].value_counts(dropna=False))
+
+#####
+# Set "maybe" to be "no"
+dogs.loc[dogs["likes_children"] == "maybe", "likes_children"] = "no"
+
+# Print out categories
+print(dogs["likes_children"].cat.categories)
+
+# Print the frequency table
+print(dogs["likes_children"].value_counts())
+
+# Remove the `"maybe" category
+dogs["likes_children"] = dogs["likes_children"].cat.remove_categories(["maybe"])
+print(dogs["likes_children"].value_counts())
+
+# Print the categories one more time
+print(dogs["likes_children"].cat.categories)
