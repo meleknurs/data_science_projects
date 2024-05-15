f49504a6e17f9968f7496b4aa208161743d88bff
Meleknur <melek.soylu@gmail.com>
2024-05-15 15:09:40 +0200

    exercise for rename categories

---
+++ exercise-data/15.05/update_categories.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+dogs=pd.read_csv("ShelterDogs.csv")
+
+# Change dtype as category
+dogs['likes_children'] = dogs['likes_children'].astype('category')
+
+# Create the my_changes dictionary
+my_changes={"Maybe?" : "Maybe"}
+
+# Rename the categories listed in the my_changes dictionary
+dogs["likes_children"] = dogs["likes_children"].cat.rename_categories(my_changes)
+
+# Use a lambda function to convert all categories to uppercase using upper()
+dogs["likes_children"] =  dogs["likes_children"].cat.rename_categories(lambda c: c.upper())
+
+# Print the list of categories
+print(dogs["likes_children"].cat.categories)
